ef308c0fe3cbc0a3dfbb8d23201d0cb89d2a5463
makih <meshman1313@gmail.com>
2018-10-04 09:26:11 -0500

    Cleaned unnecisary comments and variables.

--- template/Blink.cpp
+++ template/Blink.cpp
@@ -1,35 +1,15 @@
-/*
- * Blink.cpp
- *
- * Blinks the onboard LED on most Arduino boards.
- *
- * Andy Dalton
- * December 1, 2013
- */
 #include "Arduino.h"                                                            
 
 enum {
     // Pin number for the LED
-    LED_PIN     =   13,
-
-    // Time (in ms) between LED state changes
-    BLINK_DELAY = 1000,
+    LED_PIN = 13,
 };
 
-/*
- * setup
- *
- * Perform one-time setup for this program.
- */
 void setup() {
-    // No setup needed
+	// Sets pin to output for LED.
+    pinMode(LED_PIN, OUTPUT);
 }
 
-/*
- * loop
- *
- * Called repeatedly from the main loop.
- */
 void loop() {
     // Is the LED currently on or off?
     static boolean onOff = false;
@@ -40,6 +20,5 @@ void loop() {
     // Get ready for the next invocation of this functions
     onOff = !onOff;
 
-    // Wait a while before returning
-    delay(BLINK_DELAY);
+    delay(1000);
 }
